7fc7016ad4d4eca05ba1753cbacf3c50dff27516
Connor <couch3ater@gmail.com>
2017-09-01 21:41:17 -0400

    box now updates with 'lore'

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -3,26 +3,26 @@ $(document).ready(function(){
 	$('.hero-card').click(function(e){
 		var heroClicked = $(this).attr('class').split(/\s+/)[1];
 		var cardID = $(this).attr('id');
+		var heroBio = null;
 		$('#hero-lore').addClass('open');
 		$('#profile-pic').addClass(heroClicked);
 		
 		$.getJSON("https://couch3ater.github.io/paragonLoreGrid/js/heroes.json")
 	    	.done(function( data ) {
-	    		/*
 	    		for(homie in data.heroes.hero){
 	    			if(heroClicked == data.heroes.hero[homie].name){
-	    				alert("you selected: " + data.heroes.hero[homie].name);
+	    				heroBio = data.heroes.hero[homie].bio;
+	    				$('#bio').html(heroBio);
+	    				break;
 	    			}
 	    		}
-	    		*/
-	    		console.log(data.heroes.hero.length);
 			});
 		});
 
 	$('#hero-lore').click(function(){
 		this.className = '';
 		$('#profile-pic').removeClass();
-		//$('#bio').html();
+		$('#bio').html();
 	});
 
 });
